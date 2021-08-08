e28586489084d4f8accd5115edb9b97e7b6329f5
无声 <ivestszheng@qq.com>
2021-08-08 21:08:27 +0800

    feat: 推荐歌单组件

---
+++ src/components/recommended-music-list/index.vue
@@ -0,0 +1,53 @@
+<!--
+ * @Descripttion: 推荐歌单
+ * @Date: 2021-08-08 17:44:07
+ * @LastEditTime: 2021-08-08 20:00:50
+-->
+<template>
+  <div>
+    <div class="head">
+      <span>推荐歌单</span>
+      <span>更多></span>
+    </div>
+    <div class="items">
+      <div class="item" v-for="(item, i) in recommendedMusicList" :key="i">
+        <img :src="item.picUrl" :alt="item.name" />
+        <div class="title">{{ item.name }}</div>
+        <div class="count">{{ item.playCount }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang='ts'>
+import { getRecommendedMusicList } from '@/utils/api'
+
+export default {
+  data() {
+    return {
+      recommendedMusicList: []
+    }
+  },
+  mounted: async function () {
+    let res = await getRecommendedMusicList()
+    this.recommendedMusicList = res.data.result
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.head{
+  display: flex;
+  justify-content: space-between;
+}
+.items {
+  display: flex;
+  overflow-x: auto;
+  .item {
+    img {
+      width: 100%;
+      height: auto;
+    }
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,13 @@
 <!--
  * @Descripttion: 首页
  * @Date: 2021-06-29 21:54:24
- * @LastEditTime: 2021-08-08 16:32:00
+ * @LastEditTime: 2021-08-08 17:45:35
 -->
 <template>
   <top-nav />
   <banner />
   <icon-list />
+  <recommended-music-list />
 </template>
 
 <script lang="ts">
@@ -14,12 +15,15 @@ import { defineComponent } from 'vue'
 import banner from '@/components/banner/index.vue'
 import topNav from '@/components/top-nav/index.vue'
 import iconList from '@/components/icon-list/index.vue'
+import recommendedMusicList from '@/components/recommended-music-list/index.vue'
+
 export default defineComponent({
   name: 'App',
   components: {
     banner,
     topNav,
-    iconList
+    iconList,
+    recommendedMusicList
   }
 })
 </script>
