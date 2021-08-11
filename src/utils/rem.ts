9f767389f15c26f015e6aa77a3228dd23f014823
无声 <ivestszheng@qq.com>
2021-08-11 23:05:25 +0800

    fix: 解决TS校验报错

--- src/utils/rem.ts
+++ src/utils/rem.ts
@@ -1,7 +1,7 @@
 /*
  * @Descripttion: 实现自适应 REM 布局
  * @Date: 2021-08-01 19:00:10
- * @LastEditTime: 2021-08-01 20:54:48
+ * @LastEditTime: 2021-08-11 23:05:12
  */
 function remSize() {
     var deviceWidth = document.documentElement.clientWidth || window.innerWidth
@@ -12,7 +12,7 @@ function remSize() {
         deviceWidth = 320
     }
     document.documentElement.style.fontSize = (deviceWidth / 7.5) + 'px'
-    document.querySelector('body').style.fontSize = 0.3 + 'rem'
+    document.getElementsByTagName("body")[0].style.fontSize = 0.3 + 'rem'
 }
 
 remSize()
