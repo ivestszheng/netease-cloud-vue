d9fa6d84d5917dc683fe5a68032e387c03df36e4
无声 <ivestszheng@qq.com>
2021-09-09 23:32:32 +0800

    feat: 歌单页面滤镜处理

---
+++ src/components/list-view-top/index.vue
@@ -0,0 +1,72 @@
+<!--
+ * @Descripttion:
+ * @Date: 2021-08-23 22:54:07
+ * @LastEditTime: 2021-09-09 23:31:46
+-->
+<template>
+  <div class="listViewTop">
+    <img class="bg" :src="playlist.coverImgUrl" :alt="playlist.name" />
+    <div class="left">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-arrowLeft" />
+      </svg>
+      <div class="title">歌单</div>
+    </div>
+    <div class="right">
+      <svg class="icon search" aria-hidden="true">
+        <use xlink:href="#icon-sousuo" />
+      </svg>
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-liebiao" />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['playlist'],
+  mounted() {
+    console.log('12312321', this.playlist);
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.icon {
+  width: 0.5rem;
+  height: 0.5rem;
+  fill: #fff;
+}
+.listViewTop {
+  display: flex;
+  justify-content: space-between;
+  height: 1.2rem;
+  align-items: center;
+  font-size: .35rem;
+  .bg {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 7.5rem;
+    height: auto;
+    z-index: -1;
+    filter: blur(40px);
+  }
+  .left,
+  .right {
+    color: #fff;
+    display: flex;
+  }
+  .left {
+    .title{
+      margin-left: 0.4rem;
+    }
+  }
+  .right{
+    .search{
+      margin-right: .5rem;
+    }
+  }
+}
+</style>
--- src/views/ListView/index.vue
+++ src/views/ListView/index.vue
@@ -1,31 +1,38 @@
 <!--
  * @Descripttion: 歌单详情
  * @Date: 2021-08-22 23:27:56
- * @LastEditTime: 2021-08-22 23:52:03
+ * @LastEditTime: 2021-09-09 23:12:00
 -->
 <template>
-   <div>123123</div>
+  <div class="listView">
+    <list-view-top :playlist="state.playlist" />
+    {{state.playlist.commentCount}}
+  </div>
 </template>
 
 <script lang='ts'>
-import {onMounted, reactive} from 'vue'
-import {getMusicListDetail} from '@/utils/api'
+import { onMounted, reactive } from 'vue'
+import { getMusicListDetail } from '@/utils/api'
 import { useRoute } from 'vue-router'
+import ListViewTop from '@/components/list-view-top/index.vue'
 
 export default {
-   setup(){
-     const route = useRoute()
-     console.log('route',route);
-
-     let state = reactive({list:[]})
-     let id = route.query.id
-     onMounted(async ()=>{
-       let res = await getMusicListDetail(id)
-       console.log(res)
-     })
-   }
+  setup() {
+    const route = useRoute()
+    let state = reactive({ list: [], playlist: {} })
+    let id = route.query.id
+    onMounted(async () => {
+      let res = await getMusicListDetail(id)
+      state.playlist = res.data.playlist
+      console.log(res)
+    })
+      return {state}
+  },
+  components: {
+    ListViewTop,
+  },
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
 </style>
