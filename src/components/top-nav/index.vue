eea95b03ac866afc973acd1f040141e0685c9e7e
无声 <ivestszheng@qq.com>
2021-09-16 23:41:31 +0800

    pref: 样式

--- src/components/top-nav/index.vue
+++ src/components/top-nav/index.vue
@@ -1,49 +1,50 @@
 <!--
  * @Descripttion: 头部导航
  * @Date: 2021-08-01 22:39:21
- * @LastEditTime: 2021-09-14 22:02:27
+ * @LastEditTime: 2021-09-16 23:31:12
 -->
 <template>
-    <div class="topNav">
-        <div class="topLeft">
-            <svg class="icon" aria-hidden="true">
-                <use xlink:href="#icon-liebiao" />
-            </svg>
-        </div>
-        <div class="topCenter">
-            <span class="navBtn">我的</span>
-            <span class="navBtn active">发现</span>
-            <span class="navBtn">云村</span>
-            <span class="navBtn">视频</span>
-        </div>
-        <div class="topRight" @click="$router.push('/searchview')">
-            <svg class="icon" aria-hidden="true">
-                <use xlink:href="#icon-sousuo" />
-            </svg>
-        </div>
+  <div class="topNav">
+    <div class="topLeft">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-liebiao" />
+      </svg>
     </div>
+    <div class="topCenter">
+      <span class="navBtn">我的</span>
+      <span class="navBtn active">发现</span>
+      <span class="navBtn">云村</span>
+      <span class="navBtn">视频</span>
+    </div>
+    <div class="topRight" @click="$router.push('/searchview')">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-sousuo" />
+      </svg>
+    </div>
+  </div>
 </template>
 
 <script lang='ts'>
 export default {
-    setup() {
-    }
+  setup() {}
 }
 </script>
 
 <style lang="less" scoped>
-.topNav{
-    height: 1rem;
+.topNav {
+  height: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .topCenter {
+    width: 4.5rem;
     display: flex;
-    justify-content: space-between;
+    justify-content: space-around;
     align-items: center;
-    .topCenter{
-        width: 4.5rem;
-        display: flex;
-        justify-content: space-around;
-        .active{
-            font-weight: 900;
-        }
+    .active {
+      font-weight: 600;
+      font-size: 0.4rem;
     }
+  }
 }
 </style>
