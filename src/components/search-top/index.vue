b073f752040c86bf5826b27ece4fe3714a5d2364
无声 <ivestszheng@qq.com>
2021-09-16 22:12:42 +0800

    feat: 音乐搜索功能实现

--- src/components/search-top/index.vue
+++ src/components/search-top/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Descripttion:
  * @Date: 2021-08-23 22:54:07
- * @LastEditTime: 2021-09-16 15:14:06
+ * @LastEditTime: 2021-09-16 22:09:49
 -->
 <template>
   <div class="searchTop">
@@ -21,32 +21,91 @@
     </div>
     <div class="history">
       <div class="historyLeft">历史记录</div>
+      <div @click="clearStorage">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-lajitong" />
+        </svg>
+      </div>
     </div>
   </div>
   <div class="historyContent">
-    <div class="keywordItem" v-for="(item, i) in keywordList" :key="i">
+    <div
+      class="keywordItem"
+      v-for="(item, i) in keywordList"
+      :key="i"
+      @click="searchHistoryContent(item)"
+    >
       {{ item }}
+    </div>
+  </div>
+  <div class="list">
+    <div class="playItem" v-for="(item, i) in songs" :key="i">
+      <div class="left">
+        <div class="index">{{ i + 1 }}</div>
+        <div class="content">
+          <div class="title">{{ item.name }}</div>
+          <div class="author">
+            <span>{{ item.album.name }}</span>
+          </div>
+        </div>
+      </div>
+      <div class="right">
+        <svg class="icon" aria-hidden="true" @click="setPlayIndex(i)">
+          <use xlink:href="#icon-shipinbofangyingpian" />
+        </svg>
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-caozuo" />
+        </svg>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { searchMusic } from '@/utils/api'
+
 export default {
   name: 'search-top',
   data() {
     return {
       placeholder: '陈奕迅',
       keywordList: [],
-      searchKeyword: ''
+      searchKeyword: '',
+      searchSongs: [],
+      songs: []
     }
   },
   beforeMount() {
     this.keywordList = localStorage.keywordList ? JSON.parse(localStorage.keywordList) : []
   },
   methods: {
-    saveKeyword() {
-      this.keywordList.push(this.searchKeyword)
+    saveKeyword: async function () {
+      // 判断搜索内容是否重复
+      if (localStorage.keywordList) {
+        let tempArr = JSON.parse(localStorage.keywordList)
+        let flag = true
+        for (let i = 0; i < tempArr.length; i++) {
+          const ele = tempArr[i];
+          if (ele == this.searchKeyword) {
+            flag = false
+            break
+          }
+        }
+        if (flag) {
+          this.keywordList.unshift(this.searchKeyword)
+        }
+      }
       localStorage.keywordList = JSON.stringify(this.keywordList)
+      let result = await searchMusic(this.searchKeyword)
+      this.songs = result.data.result.songs
+    },
+    clearStorage() {
+      localStorage.removeItem('keywordList')
+      this.keywordList = []
+    },
+    searchHistoryContent(item) {
+      this.searchKeyword = item
+      this.saveKeyword()
     }
   },
 }
@@ -56,6 +115,7 @@ export default {
 .searchTop {
   width: 100vw;
   padding: 0 0.4rem;
+  margin-bottom: 0.1rem;
   .searchTopNav {
     display: flex;
     width: 100%;
@@ -71,6 +131,10 @@ export default {
     }
   }
   .history {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-right: 0.6rem;
     .historyLeft {
       font-weight: 900;
       margin-bottom: 0.25rem;
@@ -83,7 +147,8 @@ export default {
   margin-left: 0.4rem;
   width: 100vw;
   overflow: scroll;
-  direction: rtl;
+  margin-bottom: 0.2rem;
+  min-height: 0.5rem;
   .keywordItem {
     margin-right: 0.25rem;
     background: rgba(128, 128, 128, 0.1);
@@ -91,11 +156,12 @@ export default {
     line-height: 0.5rem;
     border-radius: 16px;
     padding: 0 0.25rem;
+    white-space: nowrap;
   }
 }
-  .keywordItem:last-child{
-    margin-right: .5rem;
-  }
+.keywordItem:last-child {
+  margin-right: 0.5rem;
+}
 .historyContent::-webkit-scrollbar {
   display: none;
 }
@@ -105,5 +171,44 @@ export default {
 }
 :deep(.van-field__left-icon) {
   display: none;
+}
+.list {
+  background-color: #fff;
+  padding: 0 0.3rem;
+  .playItem {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 0.1rem;
+    .left {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .index {
+        width: 0.1rem;
+        color: #ccc;
+      }
+      .content {
+        margin-left: 0.4rem;
+        .title {
+          font-size: 0.325rem;
+        }
+        .author {
+          font-size: 0.2rem;
+          color: #ccc;
+        }
+      }
+    }
+    .right {
+      width: 1rem;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .icon {
+        width: 0.325rem;
+        height: 0.325rem;
+        fill: #ccc;
+      }
+    }
+  }
 }
 </style>
