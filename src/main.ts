19bac969f1932fa515a2872ab4a89c43f02c6275
无声 <ivestszheng@qq.com>
2021-09-17 21:55:14 +0800

    perf

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 /*
  * @Descripttion:
  * @Date: 2021-06-29 21:54:24
- * @LastEditTime: 2021-09-14 23:03:32
+ * @LastEditTime: 2021-09-17 21:54:50
  */
 import { createApp } from 'vue'
 import App from './App.vue'
 import Router from './router/index'
-import { Icon,Button, Toast, Swipe, SwipeItem, Tabbar,TabbarItem, Search,Grid, GridItem,Search  } from 'vant'
+import { Icon,Button, Toast, Swipe, SwipeItem, Tabbar,TabbarItem, Search,Grid, GridItem  } from 'vant'
 import $store from '@/store/index'
 
 createApp(App)
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,12 @@
+/*
+ * @Descripttion:
+ * @Date: 2021-06-29 21:54:24
+ * @LastEditTime: 2021-09-17 21:55:04
+ */
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
 }
 declare module 'main.ts'
+declare module '@/store/index'
